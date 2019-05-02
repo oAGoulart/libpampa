e1cd1f971a92fab14c901e42eab65765e2fbfea7
oAGoulart <26435937+oAGoulart@users.noreply.github.com>
2019-05-02 13:19:59 -0400

    Added more file handling functions
    Extended the file handling opitions, tho still not completed yet

--- samples/sample.c
+++ samples/sample.c
@@ -4,8 +4,11 @@
 /* entry point */
 int main()
 {
+	/* create file */
+	create_file("/home/agoulart/MEGAsync/Repositories/loader/samples/test.test");
+
 	/* open file */
-	file_t* file = open_file("/home/agoulart/MEGAsync/Backup/calc.c");
+	file_t* file = open_file("/home/agoulart/MEGAsync/Repositories/loader/samples/test.test");
 
 	if (file != NULL) {
 		printf("%llu\n", file->size);
@@ -13,6 +16,17 @@ int main()
 		/* load file's data into memory */
 		if (load_file_data(file, 0, file->size))
 			printf("%s\n", file->data.buffer);
+
+		/* allocate memory */
+		data_t data;
+		data.size = 10;
+		data.buffer = (uint8_t*)"0123456789";
+
+		/* replace file buffer data */
+		replace_buffer_data(file, &data);
+
+		/* replace file data */
+		replace_file_data(file, 0, file->size);
 
 		printf("%llu\n", file->data.size);
 
