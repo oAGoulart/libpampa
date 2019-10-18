d0f86c25ff44ef2cd9f90f88e1654473c43a4020
oAGoulart <26435937+oAGoulart@users.noreply.github.com>
2019-10-17 23:10:57 -0300

    Enhance and improve comments

--- include/memory.h
+++ include/memory.h
@@ -52,7 +52,12 @@ typedef struct data_s
 extern "C" {
 #endif
 
-/* free data memory */
+/**********************************************************
+ * Free a block of data.
+ *
+ * Args:
+ *   data => data struct reference
+ **********************************************************/
 void data_free(data_t* data)
 {
 	if (data != NULL) {
@@ -65,10 +70,18 @@ void data_free(data_t* data)
 	}
 }
 
-/* allocate data memory */
+/**********************************************************
+ * Allocate a block of data.
+ *
+ * Args:
+ *   data => data struct reference with information for the
+ *           allocation
+ *
+ * Return:
+ *   bool => wheter or not data was allocated
+ **********************************************************/
 bool data_alloc(data_t* data)
 {
-	/* allocate memory */
 	ubyte_t* temp_ptr = NULL;
 
 	if ((temp_ptr = realloc(data->address, data->size)) != NULL) {
@@ -80,7 +93,39 @@ bool data_alloc(data_t* data)
 	return false;
 }
 
-/* set memory protection */
+/**********************************************************
+ * Copy a block of data to another.
+ *
+ * Args:
+ *   source => data to be copied
+ *   destination => where to store data
+ *
+ * Return:
+ *   bool => wheter or not data was copied
+ **********************************************************/
+bool data_copy(data_t* source, data_t* destination)
+{
+	if (source != NULL && destination != NULL) {
+		destination->size = source->size;
+
+    if (data_alloc(destination)) {
+      memcpy(destination->address, source->address, source->size);
+      
+      return true;
+    }
+	}
+
+	return false;
+}
+
+/**********************************************************
+ * Set a block of system memory protection.
+ *
+ * Args:
+ *   address => block of memory start address
+ *   size => size of the block of memory
+ *   mode => protection mode to be set
+ **********************************************************/
 void memory_set_protection(const void* address, const size_t size, const int mode)
 {
 	if (address != NULL && size > 0) {
@@ -89,19 +134,24 @@ void memory_set_protection(const void* address, const size_t size, const int mod
 
 		VirtualProtect((LPVOID)address, size, mode, &old_mode);
 #else
-		/* get size of pages */
 		long page_sz = sysconf(_SC_PAGESIZE);
 
-		/* find page pointer */
 		void* page_ptr = (long*)((long)address & ~(page_sz - 1));
 
-		/* change protection */
 		mprotect(page_ptr, size, mode);
 #endif
 	}
 }
 
-/* get memory protection */
+/**********************************************************
+ * Get a block of system memory protection.
+ *
+ * Args:
+ *   address => block of memory start address
+ *
+ * Return:
+ *   int => system memory protection mode
+ **********************************************************/
 int memory_get_protection(const void* address)
 {
 	int result = 0;
@@ -113,7 +163,7 @@ int memory_get_protection(const void* address)
 		if (VirtualQuery(address, &page_info, sizeof(MEMORY_BASIC_INFORMATION)) == sizeof(MEMORY_BASIC_INFORMATION))
 			result = page_info.Protect;
 #else
-		FILE* maps = fopen("/proc/self/maps", "r"); /* currently mapped memory regions */
+		FILE* maps = fopen("/proc/self/maps", "r");
 
 		if (maps != NULL) {
 			ulong_t block[2] = { 0, 0 }; /* block of memory addresses */
@@ -134,7 +184,6 @@ int memory_get_protection(const void* address)
 				}
 			}
 
-			/* close file */
 			fclose(maps);
 		}
 #endif
@@ -143,21 +192,25 @@ int memory_get_protection(const void* address)
 	return result;
 }
 
-/* write data into memory */
+/**********************************************************
+ * Write a block of system memory.
+ *
+ * Args:
+ *   address => block of memory start address
+ *   data => data to be written
+ *   size => size of the data
+ *   vp => is virtual protection enabled?
+ **********************************************************/
 void memory_set_raw(void* address, const void* data, const size_t size, const bool vp)
 {
 	if (address != NULL && data != NULL && size > 0) {
 		if (vp) {
-			/* store current memory protection */
 			int old_mode = memory_get_protection(address);
 
-			/* remove virtual protection */
 			memory_set_protection(address, size, MEM_UNPROT);
 
-			/* write data into memory */
 			memcpy(address, data, size);
 
-			/* reset virtual protection */
 			memory_set_protection(address, size, old_mode);
 		}
 		else
@@ -166,14 +219,22 @@ void memory_set_raw(void* address, const void* data, const size_t size, const bo
 }
 
 #ifndef __WINDOWS__
-/* get process base address */
+/**********************************************************
+ * Get the base address of a process.
+ *
+ * Args:
+ *   pid => process id
+ *
+ * Return:
+ *   ulong_t => base address
+ **********************************************************/
 ulong_t process_get_base_address(const pid_t pid)
 {
 	ulong_t base_addr = 0x0; /* base address */
 	char path[FILENAME_MAX]; /* path to file */
 
 	if (sprintf(path, "/proc/%d/maps", pid) >= 0) {
-		FILE* maps = fopen(path, "r"); /* process mapped memory regions */
+		FILE* maps = fopen(path, "r");
 
 		if (maps != NULL) {
 	#ifdef __X86_ARCH__
