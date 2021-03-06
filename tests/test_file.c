1dcbd29b42822d106edf76fbd94ca5a5897d90da
Augusto Goulart <26435937+oAGoulart@users.noreply.github.com>
2021-03-06 11:37:26 -0300

    Add test for file open function

--- tests/test_file.c
+++ tests/test_file.c
@@ -9,7 +9,22 @@ void test_file_create()
   TEST_CHECK(file_create("../test.log") == true);
 }
 
+void test_file_open()
+{
+  file_t* file = NULL;
+
+  TEST_CHECK(file_open(&file, NULL) == false);
+  TEST_CHECK(file_open(NULL, "test.log") == false);
+  TEST_CHECK(file_open(NULL, NULL) == false);
+  TEST_CHECK(file_open(&file, "filethatdontexist.log") == false);
+
+  file_create("filethatexist.log");
+  TEST_CHECK(file_open(&file, "filethatexist.log") == true);
+  file_close(file);
+}
+
 TEST_LIST = {
   { "file_create", test_file_create },
+  { "file_open", test_file_open },
   { NULL, NULL }
 };
